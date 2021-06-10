906af3d697cbd972d7bc24541c28b3e5f9ba3655
Ben Oommen <epsilon@Mathews-MacBook-Pro.local>
2021-06-10 08:54:07 -0500

    restructure replay buffer

--- models.py
+++ models.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.distributions import Normal
+import numpy as np
+
+LOG_STD_MAX = 2
+LOG_STD_MIN = -20
 
 class QNetwork(nn.Module):
     def __init__(self, num_observations, num_actions, hidden_dim):
@@ -19,17 +24,62 @@ class QNetwork(nn.Module):
     def forward(self, state, action):
         input = torch.cat([state, action], 1)
 
-        x1 = F.relu()
+        x1 = F.relu(self.q1_linear1(input))
+        x1 = F.relu(self.q1_linear2O(x1))
+        x1 = self.q1_linear3(x1)
 
+        x2 = F.relu(self.q2_linear1(input))
+        x2 = F.relu(self.q2_linear2(x2))
+        x2 = self.q2_linear3(x2)
 
+        return x1, x2
 
 
 class GuassianPolicy(torch.nn.Module):
-    def __init__(self, num_observations, num_actions, hidden_dim):
-        super(GaussianPolicy, self).__init__()
+    def __init__(self, num_observations, num_actions, hidden_dim, act_limit):
+        super().__init__()
 
-        self.linear1 = nn.Linear(num_observations, hidden_dim)
-        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+        # 3 layer network, with 2 outputs
 
-        self.mean_linear = nn.Linear(hidden_dim, num_actions)
-        self.cov_linear = nn.Linear(hidden_dim, num_actions)
+        self.layer1 = nn.Linear(num_observations, hidden_dim)
+        self.layer2 = nn.Linear(hidden_dim, hidden_dim)
+
+        self.mean_layer = nn.Linear(hidden_dim, num_actions)
+        self.log_stddev_layer = nn.Linear(hidden_dim, num_actions)
+
+        self.act_limit = act_limit
+
+        # apply initial weights
+    
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        mean = self.mean_layer(x)
+        log_stddev = self.log_stddev_layer(x)
+
+        return mean, log_stddev
+
+    def sample(self, state):
+        mean, log_stddev = self.forward(state)
+        stddev = log_stddev.exp()
+        normal = Normal(mean, stddev)
+        action = torch.tanh(normal.rsample())
+
+        log_prob = normal.log_prob(action).sum(axis=1)
+        log_prob -= (2*(np.log(2) - action - F.softplus(-2*action))).sum(axis=1)
+
+
+        return action * self.act_limit, log_prob
+
+class ActorCritic(nn.Module):
+    def __init__(self, num_observations, num_actions, act_limit, hidden_dim=(256,256)):
+        super().__init__()
+
+        self.policy = GuassianPolicy(num_observations, num_actions, hidden_dim, act_limit)
+        self.q1 = QNetwork(num_observations, num_actions, hidden_dim)
+        self.q1 = QNetwork(num_observations, num_actions, hidden_dim)
+    
+    def act(self, observation):
+        with torch.nograd():
+            action, _ = self.policy(observation)
+            return action.numpy()
--- sac.py
+++ sac.py
@@ -1,15 +1,27 @@
 import torch
-from models import QNetwork
+
+from replay_buffer import ReplayBuffer
+from models import ActorCritic
+
+from copy import deepcopy
+import itertools
 
 class SAC:
-    def __init__(self, observations, actions, args):
-        """
-        observations: list of observations from environment
-        actions: list of possible actions for agent
-        args: gamma (discount factor), alpha (temperature coeff)
-        """
-
+    def __init__(self, num_observations, actions, args):
         self.gamma = args.gamma
+        self.tau = args.tau
         self.alpha = args.alpha
 
-        self.critic = QNetwork()
+        self.act_limit = args.act_limit
+
+        self.ac = ActorCritic(num_observations, actions, self.act_limit)
+        self.ac_target = deepcopy(self.ac)
+
+        # Freeze target networks with respect to optimizers (only update via polyak averaging)
+        for param in self.ac_target.parameters():
+            param.requires_grad = False
+        
+        self.q_parameters = itertools.chain(self.ac.q1.parameters(), self.ac.q2.parameters())
+
+        replay_buffer = ReplayBuffer(num_observations, actions, args.replay_size)
+        
